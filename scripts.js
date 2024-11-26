2217a8977cedda4a0725e1e493d44785621a4e2b
MZhafran <muhd.zhafran@gmail.com>
2024-11-26 14:40:56 +0800

    make changes for mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@
 const ctx = document.getElementById('radarChart').getContext('2d');
 
 const data = {
-  labels: ['Python','JavaScript','SQL/PostgreSQL','Git',
+  labels: ['Python','JavaScript','SQL/PostgreSQL','Git','Plotly Dash (Web-based Dashboard)',
     'Power BI (Including DAX)', 'Tableau','Excel','Data Visualization',
     'Machine Learning (Including ML Deployment)','Building ETL Data Pipelines','PySpark (Big Data Processing)','Statistics/Econometrics'],
   datasets: [{
     label: 'My Data Analytics Skills Proficiency',
-    data: [8.5, 1.5, 8.5,5, 8,
+    data: [8.5, 1.5, 8.5,5,7,8,
          7.5,8,9,4.5,7.5,6,7.5],
     backgroundColor: 'rgba(168, 169, 175, 0.3)',
     borderColor: 'rgba(0, 0, 0, 1)', // Colour for the line
@@ -19,6 +19,7 @@ const data = {
 };
 
 const options = {
+  responsive: true,
   scales: {
     r: {
       beginAtZero: true,
@@ -46,4 +47,13 @@ new Chart(ctx, {
   type: 'radar',
   data: data,
   options: options
+});
+
+
+window.addEventListener('resize', () => {
+  const newFontSize = getResponsiveFontSize();
+  myRadarChart.options.scales.r.ticks.font.size = newFontSize;
+  myRadarChart.options.scales.r.pointLabels.font.size = newFontSize;
+  myRadarChart.options.plugins.legend.labels.font.size = newFontSize;
+  myRadarChart.update(); // Re-render the chart with updated font size
 });
